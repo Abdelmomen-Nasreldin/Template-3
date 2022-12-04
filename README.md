8f2eae891605f1b410affebd82076b571fa35a63
Abdelmomen-Nasreldin <92486538+Abdelmomen-Nasreldin@users.noreply.github.com>
2022-12-04 18:10:50 +0200

    article section done, and fetch its data from json server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Template 3 in Elzero Web School Channel but using the powerful of SCSS and JS.
+# HTML / SCSS / JS / JSON.
+# WITHOUT any Frameworks
+    - Template 3 in Elzero Web School Channel but using the powerful of SCSS and JS.
--- js/index.js
+++ js/index.js
@@ -22,3 +22,47 @@ function hideMegaMenu() {
 function hideNavMenu() {
     mainNavList.classList.remove("show");
 }
+// let articleData = [];
+// fetching data ///////////////////////
+const cardsContainer = document.querySelector("#articleSection .cards-container");
+fetch("../data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log(data.articleData);
+        getCardsDataHandler(data.articleData, cardsContainer, elementDataHandler)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+ 
+
+function getCardsDataHandler(data, dataContainer, innerDataFun) {
+    for (const elementData of data) {
+        const card = document.createElement("div");   
+        card.id = elementData.id;
+        innerDataFun(card ,elementData)
+        dataContainer.append(card);
+    }
+    console.log("done");
+}
+
+function elementDataHandler(element, data){
+    element.classList.add("card");
+    element.innerHTML = `
+    <div class="image">
+        <img src="${data.src}" alt="card Image">
+    </div>
+    <div>
+        <div class="text">
+            <h3>${data.title}</h3>
+            <p>${data.text}</p>
+        </div>
+
+        <button class="read-more">
+            <span>Read More</span>
+            <i class="fa-solid fa-arrow-right"></i>
+        </button>
+    </div>
+    `
+}
