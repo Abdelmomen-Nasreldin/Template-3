d20cc2faaa4cf1771066a20618f5d212670b862a
Abdelmomen-Nasreldin <92486538+Abdelmomen-Nasreldin@users.noreply.github.com>
2023-01-11 16:31:50 +0200

    reactor

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # HTML / SCSS / JS / JSON.
-# WITHOUT Any Frameworks
+# WITHOUT Any Frameworks or Library
     - preview : https://momen-template3.netlify.app/
     - Template 3 in Elzero Web School Channel With using the powerful of SCSS and JS.
     - Fetching data from JSON file.
--- js/index.js
+++ js/index.js
@@ -6,10 +6,12 @@ const megaMenu = document.getElementById("megaMenu");
 
 toggleIcon.addEventListener("click", toggleNavMenu);
 welcomeSection.addEventListener("click", hideNavMenu);
-otherLinks.addEventListener("mouseover", showMegaMenu);
-otherLinks.addEventListener("mouseleave", hideMegaMenu);
-megaMenu.addEventListener("mouseover", showMegaMenu);
-megaMenu.addEventListener("mouseleave", hideMegaMenu);
+
+[otherLinks, megaMenu].forEach((item) => {
+    item.addEventListener("mouseover", showMegaMenu);
+    item.addEventListener("mouseleave", hideMegaMenu);
+});
+
 function toggleNavMenu() {
     mainNavList.classList.toggle("show");
 }
@@ -22,30 +24,44 @@ function hideMegaMenu() {
 function hideNavMenu() {
     mainNavList.classList.remove("show");
 }
-// let articleData = [];
-// fetching data ///////////////////////
-const cardsContainer = document.querySelector(
-    "#articleSection .cards-container"
-);
-fetch("./js/data.json")
-    .then((response) => response.json())
-    .then((data) => {
-        const articleContainer = document.querySelector(
-            "#articleSection .cards-container"
-        );
 
-        elementDataHandler(articleContainer, data.articleData);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+// fetching data ///////////////////////
+const fetchFun = async () => {
+    try {
+        const response = await fetch("./js/data.json");
+
+        if (response.status !== 200) {
+            throw new Error("can't fetch data.");
+        }
+        const data = await response.json();
+
+        const articleContainer = sectionContainerHandler("articleSection")
+        const featuresContainer = sectionContainerHandler("featuresSection")
+        const testimonialsContainer = sectionContainerHandler("testimonialsSection")
+        const teamMembersContainer = sectionContainerHandler("teamMembersSection")
+   
+        elementDataHandler(articleContainer, data[0].articleData);
+        elementDataHandler(featuresContainer, data[2].featuresData);
+        elementDataHandler(testimonialsContainer, data[3].testimonialsData);
+        elementDataHandler(teamMembersContainer, data[4].teamMembersData);
+
+    } catch (err) {
+        console.log("error: ", err.message);
+    }
+}
+
+fetchFun();
+
+function sectionContainerHandler(sectionId) {
+    const sectionContainer = document.querySelector(`#${sectionId} .cards-container`)
+    return sectionContainer
+}
 
 function elementDataHandler(ParentElement, source) {
     const element = ParentElement.querySelector(".card-template");
 
     for (let obj of source) {
         const card = document.importNode(element.content, true);
-        console.log(card);
         obj.imgSrc
             ? (card.querySelector("img").src = `../img/${obj.imgSrc}`)
             : (card.querySelector("img").src = "../img/welcome-image.png");
@@ -61,133 +77,8 @@ function elementDataHandler(ParentElement, source) {
         obj.desc
             ? (card.querySelector(".content p").innerText = obj.desc)
             : (card.querySelector(".content p").innerText =
-                  "the text is not found so you see the default one");
+                "the text is not found so you see the default one");
 
         ParentElement.append(card);
     }
 }
-
-let teamMembersData = [
-    {
-        imgSrc: "team-01.jpg",
-        title: "Hady",
-        desc: "great hard worker",
-    },
-    {
-        imgSrc: "team-02.jpg",
-        title: "Momen",
-        desc: "great hard worker",
-    },
-    {
-        imgSrc: "team-03.jpg",
-        title: "Ahmed",
-        desc: "great hard worker",
-    },
-    {
-        imgSrc: "team-04.jpg",
-        title: "Mohamed",
-        desc: "Manager",
-    },
-    {
-        imgSrc: "team-05.png",
-        title: "Esraa",
-        desc: "Secretary",
-    },
-    {
-        imgSrc: "team-06.png",
-        title: "Saleh",
-        desc: "great hard worker",
-    },
-    {
-        imgSrc: "team-07.jpg",
-        title: "Hadeer",
-        desc: "great hard worker",
-    },
-    {
-        imgSrc: "team-08.jpg",
-        // title: "Hady",
-        // desc: "great hard worker"
-    },
-];
-let testimonialsData = [
-    {
-        imgSrc: "avatar-01.png",
-        title: "Momen",
-        desc: "Front-end Developer",
-    },
-    {
-        imgSrc: "avatar-02.png",
-        title: "Hady",
-        desc: "Full-stack Developer",
-    },
-    {
-        imgSrc: "avatar-03.png",
-        title: "Saleh",
-        desc: "Full-stack Developer",
-    },
-    {
-        imgSrc: "avatar-04.png",
-        title: "Mohamed",
-        desc: "Manager",
-    },
-    {
-        imgSrc: "avatar-05.png",
-        title: "Mostafa",
-        desc: "Assistant",
-    },
-    {
-        imgSrc: "avatar-06.png",
-        title: "Hesham",
-        desc: "Assistant",
-    },
-];
-let featuresData = [
-    {
-        imgSrc: "features-01.jpg",
-        title: "Quality",
-        // desc: "great hard worker"
-    },
-    {
-        imgSrc: "features-02.jpg",
-        title: "Control",
-        // desc: "great hard worker"
-    },
-    {
-        imgSrc: "features-03.jpg",
-        title: "Fast",
-        // desc: "great hard worker"
-    },
-];
-let galleriesData = [
-    {
-        imgSrc: "gallery-01.png",
-    },
-    {
-        imgSrc: "gallery-02.png",
-    },
-    {
-        imgSrc: "gallery-03.png",
-    },
-    {
-        imgSrc: "gallery-04.png",
-    },
-    {
-        imgSrc: "gallery-05.jpg",
-    },
-    {
-        imgSrc: "gallery-06.png",
-    },
-];
-
-const testimonialsContainer = document.querySelector(
-    "#testimonialsSection .cards-container"
-);
-const teamMembersContainer = document.querySelector(
-    "#teamMembersSection .cards-container"
-);
-const featuresContainer = document.querySelector(
-    "#featuresSection .cards-container"
-);
-elementDataHandler(teamMembersContainer, teamMembersData);
-elementDataHandler(testimonialsContainer, testimonialsData);
-elementDataHandler(featuresContainer, featuresData);
